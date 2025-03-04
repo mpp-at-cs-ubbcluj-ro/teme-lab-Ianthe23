575cf3d97eef3dabc26fc2b394e7428d506e27b0
Ianthe23 <ivo.pastin@gmail.com>
2025-03-04 16:55:58 +0200

    lab2

---
+++ lab2/src/main/java/org/example/repo/RequestRepo.java
@@ -0,0 +1,8 @@
+package org.example.repo;
+
+import org.example.model.ComputerRepairRequest;
+
+public class RequestRepo extends AbstractRepo<ComputerRepairRequest, Integer>{
+    public RequestRepo(){}
+}
+
